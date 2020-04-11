19cf1f3fc2d1ae3224669ac099db855a842980db
JasonXBlue <JasonX.blue1@gmail.com>
2020-04-11 10:16:51 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,32 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ITodo } from "./interfaces/itodo";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent implements OnInit{
-  title = 'Todos';
-  todoList: any [] = [];
+export class AppComponent {
+  title = "Todos";
+  todoList: ITodo[] = [];
   todoTitle: string;
   todoId: number = 0;
-  ngOnInit() {
-    this.todoTitle = '';
-    this.todoList = [
-      // example of how to make an item in todo list
-      { title: 'Install Angular CLI', isDone: false },
-    
-    ];
-  }
-  addTodo():void {
+
+  ngOnInit() {}
+
+  addTodo(): void {
     this.todoList.push({
+      id: this.todoId,
       title: this.todoTitle,
-      isDone: false
+      description: "",
     });
-    
+
     // resets our todoTitle variable to an empty string
-    this.todoTitle = '';
+    this.todoTitle = "";
+    this.todoId++;
   }
-  deleteTodo(todo:any) {
-    const index = this.todoList.findIndex(todoItem => todoItem === todo);
+  deleteTodo(todo: ITodo) {
+    const index = this.todoList.findIndex((todoItem) => todoItem === todo);
     this.todoList.splice(index, 1);
   }
-
 }
